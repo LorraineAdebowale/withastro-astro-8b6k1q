a6393c643c323865eac48c3f061443e28af4a5e7
LorraineAdebowale <lorrainet85@gmail.com>
2024-05-24 06:53:07 +0100

    Day and Night Themes and Interactive Greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-banana.netlify.app/"
+  site: "https://astro-blog-banana.netlify.app/",
+  integrations: [preact()]
 });
